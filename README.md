6558ffb1d27d26ee6d93f62039658d128430ef15
David Li <davidli2010@foxmail.com>
2020-03-09 14:35:27 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# pgdatetime
+
+SQL date and time types written in Rust, compatible with PostgreSQL's date and time types.
+
+See also: [Date/Time Types](https://www.postgresql.org/docs/current/datatype-datetime.html)
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright 2020 CoD Team
+
+//! SQL date and time types written in Rust, compatible with PostgreSQL's date and time types.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
